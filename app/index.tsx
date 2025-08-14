ac307af6d544b45aee342fbf0f2493149e7c175d
shahidhustles <shahidpatel.1666@gmail.com>
2025-08-14 19:37:07 +0530

    feat: initialize project with Babel, Tailwind CSS, and NativeWind configuration
    - Add Babel configuration for Expo and NativeWind support.
    - Create constants file to manage assets and categories for the application.
    - Define TypeScript declarations for image file imports.
    - Set up Metro bundler configuration to integrate NativeWind.
    - Add TypeScript definitions for NativeWind.
    - Configure Tailwind CSS with custom theme and colors.

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,17 @@
-import { Text, View } from "react-native";
+import { offers } from "@/constants";
+import { FlatList, Pressable, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+   <SafeAreaView>
+    <FlatList data={offers} renderItem={({item, index}) => (
+      <View>
+        <Pressable className="bg-amber-600 my-3 h-48 rounded-xl">
+          <Text>{item.title}</Text>
+        </Pressable>
+      </View>
+    )}   /> 
+   </SafeAreaView>
   );
 }
